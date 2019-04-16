0da6ec448e63ae5bb6700b9bdac72183fd75f2a0
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-15 20:06:21 -0400

    Update Day 06: Let's Review.js

--- Day 06: Let's Review.js
+++ Day 06: Let's Review.js
@@ -1,6 +1,23 @@
 function processData(input) {
     //Enter your code here
-} 
+    var input = input.split('\n');
+
+    for (var i = 1; i < input.length; i++) {
+        var split_Word = input[i].split('');
+        var even = '';
+        var odd = '';
+
+        for (var x = 0; x < split_Word.length; x++) {
+            if (x % 2 == 0) {
+                var even = even + split_Word[x];
+            } 
+            else {
+                var odd = odd + split_Word[x];
+            }
+        }
+        console.log(even + ' ' + odd);
+    }
+}
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
